cab286e6ce390cc10f70f68b8781cd2e844b5e7c
NevenaN <n_milanova@mail.bg>
2018-10-08 14:34:31 +0300

    TownService.cs

---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Abstract/Contracts/ITownService.cs
@@ -0,0 +1,9 @@
+﻿using LibrarySystem.Data.Models;
+
+namespace LibrarySystem.Services.Services
+{
+    public interface ITownService
+    {
+        Town AddTown(string townName);
+    }
+}
---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Services/TownService.cs
@@ -0,0 +1,33 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySystem.Services.Abstract;
+using System.Linq;
+using System.Collections.Generic;
+using LibrarySystem.Data.Models;
+
+namespace LibrarySystem.Services.Services
+{
+    public class TownService : BaseServicesClass, ITownService
+    {
+        public TownService(ILibrarySystemContext context) : base(context)
+        {
+        }
+        public Town AddTown(string townName)
+        {
+            var town = new Town()
+            {
+                TownName = townName
+            };
+
+            var dbTown = context.Towns.SingleOrDefault(t => t.TownName == townName);
+
+            if (dbTown == null)
+            {
+                base.context.Towns.Add(town);
+                context.SaveChanges();
+            }
+
+            return town;
+        }
+
+    }
+}
