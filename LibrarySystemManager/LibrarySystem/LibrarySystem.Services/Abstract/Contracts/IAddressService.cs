7eb6b9e5dbe8ea118fd8be2015ea4bbd30920976
aniklev <mr.niklev@gmail.com>
2018-10-08 13:38:52 +0300

    Extract Address Interface

---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Abstract/Contracts/IAddressService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using LibrarySystem.Data.Models;
+
+namespace LibrarySystem.Services.Services
+{
+    public interface IAddressService
+    {
+        Address AddAddress(string streetAddress, Town town);
+        IEnumerable<Address> GetAddress(string streetAddress);
+    }
+}
--- LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Services/AddressService.cs
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Services/AddressService.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace LibrarySystem.Services.Services
 {
-    public class AddressService : BaseServicesClass
+    public class AddressService : BaseServicesClass, IAddressService
     {
         public AddressService(ILibrarySystemContext context) : base(context)
         {
