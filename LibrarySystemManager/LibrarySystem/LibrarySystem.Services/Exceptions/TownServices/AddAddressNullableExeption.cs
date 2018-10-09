42b45cbbd11acc8f562c28959e753c1c61719326
aniklev <mr.niklev@gmail.com>
2018-10-09 13:28:22 +0300

    Service layer execptions

---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Services/Exceptions/TownServices/AddAddressNullableExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrarySystem.Services.Exceptions.TownServices
+{
+    public class AddTownNullableExeption : Exception
+    {
+        public AddTownNullableExeption(string message) : base(message)
+        {
+        }
+    }
+}
