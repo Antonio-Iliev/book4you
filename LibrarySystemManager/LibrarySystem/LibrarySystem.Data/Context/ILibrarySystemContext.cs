eb203cec789a66eb67fc5b52bcdc1a96df051010
aniklev <mr.niklev@gmail.com>
2018-10-30 15:52:24 +0200

    Add Back Context Interface

---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.Data/Context/ILibrarySystemContext.cs
@@ -0,0 +1,18 @@
+﻿using LibrarySystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data.Context
+{
+    public interface ILibrarySystemContext
+    {
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<Book> Books { get; set; }
+        DbSet<Genre> Genres { get; set; }
+        DbSet<Town> Towns { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<UsersBooks> UsersBooks { get; set; }
+
+        int SaveChanges();
+    }
+}
