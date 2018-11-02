2b48ae036ff9cc1b3160ef13ad6d476999fe4f2d
NevenaN <n_milanova@mail.bg>
2018-11-02 19:53:48 +0200

    Resolved conflicts

--- LibrarySystemManager/LibrarySystem/LibrarySystem.WebClient/Controllers/HomeController.cs
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.WebClient/Controllers/HomeController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LibrarySystem.WebClient.Models;
+using LibrarySystem.Services;
 
 namespace LibrarySystem.WebClient.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IBooksServices bookService;
+
+        public HomeController(IBooksServices bookService)
+        {
+            this.bookService = bookService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var books = bookService.ListBooks().Select(b => new BookViewModel(b));
+            return View(books);
         }
 
         public IActionResult About()
