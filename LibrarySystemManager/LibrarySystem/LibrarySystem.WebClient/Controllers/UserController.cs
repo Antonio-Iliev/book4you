09c4b555278cb48ee641d273284ab4c61a914f62
Antonio Iliev <iliev.t.a@gmail.com>
2018-11-03 08:44:57 +0200

    Add UserController

---
+++ LibrarySystemManager/LibrarySystem/LibrarySystem.WebClient/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibrarySystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.WebClient.Controllers
+{
+    [Authorize(Roles = "Admin, User")]
+    public class UserController : Controller
+    {
+        private readonly IBooksServices _booksServices;
+
+        public UserController(IBooksServices booksServices)
+        {
+            this._booksServices = booksServices;
+        }
+
+        public IActionResult Index()
+        {
+            var allBooks = _booksServices.ListBooks();
+
+            return View();
+        }
+    }
+}
